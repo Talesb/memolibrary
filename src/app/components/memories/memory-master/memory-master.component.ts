60499db53ca88ce7a510731aa7ec099566621fa6
talesb <tales.batista@temasistemas.com.br>
2023-04-23 15:51:58 -0300

    Adjusting memory class using ngclass

--- src/app/components/memories/memory-master/memory-master.component.ts
+++ src/app/components/memories/memory-master/memory-master.component.ts
@@ -9,21 +9,21 @@ export class MemoryMasterComponent implements OnInit {
 
 
   memoryList = [
-    // {
-    //   content: 'Test',
-    //   author: 'Talesb',
-    //   model: 'modelo3'
-    // },
-    // {
-    //   content: 'Test 2',
-    //   author: 'Talesb 2',
-    //   model: 'modelo2'
-    // },
-    // {
-    //   content: 'Test 3',
-    //   author: 'Talesb 3',
-    //   model: 'modelo1'
-    // }
+    {
+      content: 'Test',
+      author: 'Talesb',
+      model: 'modelo3'
+    },
+    {
+      content: 'Test 2',
+      author: 'Talesb 2',
+      model: 'modelo2'
+    },
+    {
+      content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum sagittis dolor, et convallis metus. Donec id est in eros viverra tincidunt vitae rhoncus mauris. Donec lacinia nunc non sapien sodales cursus. Ut laoreet ex nec turpis egestas posuere. Cras non dolor fringilla, fringilla risus eget, finibus elit. Nullam tincidunt nunc et libero vulputate rutrum. ',
+      author: 'Talesb 3',
+      model: 'modelo1'
+    }
 
   ];
 
--- src/app/components/memories/memory/memory.component.ts
+++ src/app/components/memories/memory/memory.component.ts
@@ -8,13 +8,21 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MemoryComponent implements OnInit {
 
   @Input()
-  memory:any;
+  memory: any;
 
 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  memoryWidth(): string {
+    if (this.memory.content.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
   }
 
 }
